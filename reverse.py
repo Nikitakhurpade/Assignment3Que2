e4912fdcf5a2f55e006ac883d5206abbe0b0729d
Nikitakhurpade <124980281+Nikitakhurpade@users.noreply.github.com>
2023-02-19 13:32:32 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,10 @@
+#Write a Python program to reverse a string.
+#Sample String : "1234abcd"
+#Expected Output : "dcba4321"
+def reverse(x):
+    return x[::-1]
+a=reverse(input('Enter the string----->'))
+print('Reverse string----->',a)    
+
+
+    
